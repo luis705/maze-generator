b9662c218bd5a7ce0f954dee5ec2eb06e721f1b1
Luís Otávio Amorim <luisotaviolamorim@gmail.com>
2020-07-09 14:46:13 -0300

    Changed generator to OOP style

--- tile.py
+++ tile.py
@@ -1,75 +1,115 @@
 import pygame
-from pygame.locals import *
 
-from constants import *
-from utils import get_index
-
-from random import randint
 
 class Tile:
     def __init__(self, i, j, width, height):
+        # Basic setup
         self.i = i
         self.j = j
-        self.f = 0
-        self.g = 0
-        self.h = 0
         self.width = width
         self.height = height
+
+        #  Depth-first setup
         self.walls = [True, True, True, True]
         self.visited = False
         self.previous = None
-    
-    def show(self, screen):
+
+        # Color setup
+        self.line_color = (0, 0, 0)
+        self.highlight_color = (206, 233, 217)
+        self.visited_color = (141, 128, 173)
+
+    def show(self, win):
+        """
+        Show tile on the screen
+        """
+        # Get x and y coordinates
         x = self.i * self.width
         y = self.j * self.height
 
-        if self.walls[0]:
-            pygame.draw.line(screen, LINE_COLOR, (x, y), (x + self.width, y), 2)
-        if self.walls[1]:
-            pygame.draw.line(screen, LINE_COLOR, (x, y), (x, y + self.height), 2)
-        if self.walls[2]:
-            pygame.draw.line(screen, LINE_COLOR, (x, y + self.height), (x + self.width, y +self.height), 2)
-        if self.walls[3]:
-            pygame.draw.line(screen, LINE_COLOR, (x + self.width, y), (x + self.width, y + self.height), 2)
-        
+        # Set Color
         if self.visited:
-            pygame.draw.rect(screen, VISITED_COLOR, (x + 2, y + 2, self.width - 2, self.height - 2))
+            pygame.draw.rect(win, self.visited_color, (x + 2, y + 2, self.width - 2, self.height - 2))
 
-    def check_neighbors(self, grid):
+        # Draw walls
+        if self.walls[0]:
+            pygame.draw.line(win, self.line_color, (x, y), (x + self.width, y), 2)
+        if self.walls[1]:
+            pygame.draw.line(win, self.line_color, (x, y), (x, y + self.height), 2)
+        if self.walls[2]:
+            pygame.draw.line(win, self.line_color, (x, y + self.height),
+                             (x + self.width, y + self.height), 2)
+        if self.walls[3]:
+            pygame.draw.line(win, self.line_color, (x + self.width, y),
+                             (x + self.width, y + self.height), 2)
+
+    def highlight(self, win, bool):
+        """
+        Change color of tile to highlight it or not
+        """
+        if bool:
+            pygame.draw.rect(win, self.highlight_color, pygame.Rect(self.i * self.width + 2,
+                                                                    self.j * self.height + 2, self.width - 2, self.height - 2))
+
+        else:
+            pygame.draw.rect(win, self.visited_color, pygame.Rect(self.i * self.width + 2,
+                                                                  self.j * self.height + 2, self.width - 2, self.height - 2))
+
+    @staticmethod
+    def get_index(i, j, columns, rows):
+        """
+        Get the index of a tile on the 1 dimension list
+        Parameters:
+            i: integer -> tile column
+            j: integer -> tile row
+            columns: integer -> number of column
+            rows: integer -> number of rows
+        """
+        if i < 0 or j < 0 or i > columns - 1 or j > rows - 1:
+            return None
+        return i + j * columns
+
+    def check_neighbors(self, grid, columns, rows):
+        """
+        Get the tile neighbors
+        Parameters:
+            grid: list -> list with every tile
+            columns: integer -> number of columns on grid
+            rows: integer -> number of rows on grid
+        Return:
+            neighbors: list -> collection of at most 4 Tile objects
+        """
         neighbors = []
 
-        top_index = get_index(self.i, self.j - 1)
-        right_index = get_index(self.i - 1, self.j)
-        bottom_index = get_index(self.i, self.j + 1)
-        left_index = get_index(self.i + 1, self.j)
+        top_index = Tile.get_index(self.i, self.j - 1, columns, rows)
+        right_index = Tile.get_index(self.i - 1, self.j, columns, rows)
+        bottom_index = Tile.get_index(self.i, self.j + 1, columns, rows)
+        left_index = Tile.get_index(self.i + 1, self.j, columns, rows)
 
         if top_index:
             top = grid[top_index]
             neighbors.append(top)
-        else:
-            neighbors.append(Tile(-1, -1, -1, -1))
 
-        if  left_index:
+        if left_index:
             left = grid[left_index]
             neighbors.append(left)
-        else:
-            neighbors.append(Tile(-1, -1, -1, -1))
 
-        if right_index :
+        if right_index:
             right = grid[right_index]
             neighbors.append(right)
-        else:
-            neighbors.append(Tile(-1, -1, -1, -1))
 
-        if  bottom_index :
+        if bottom_index:
             bottom = grid[bottom_index]
             neighbors.append(bottom)
-        else:
-            neighbors.append(Tile(-1, -1, -1, -1))
 
         return neighbors
 
     def remove_walls(self, neighbor):
+        """
+        Remove the walls between to tiles
+        Parameters:
+            neighbor: Tile
+        """
         x = self.i - neighbor.i
         if x == 1:
             self.walls[1] = False
@@ -84,19 +124,3 @@ class Tile:
         elif y == -1:
             self.walls[2] = False
             neighbor.walls[0] = False
-
-    def highlight(self, screen, bool):
-        if bool:
-            pygame.draw.rect(screen, HIGHLIGHT_COLOR, pygame.Rect(self.i * self.width + 2, self.j * self.height + 2, self.width - 2, self.height - 2))
-        else:
-            pygame.draw.rect(screen, VISITED_COLOR, pygame.Rect(self.i * self.width + 2, self.j * self.height + 2, self.width - 2, self.height - 2))
-
-    def neighbor_side(self, neighbor):
-        if self.j - neighbor.j >= 1:
-            return 0
-        elif self.i - neighbor.i <= -1:
-            return 1
-        elif self.j - neighbor.j <= -1:
-            return 2
-        elif self.i - neighbor.i >= 1:
-            return 3
